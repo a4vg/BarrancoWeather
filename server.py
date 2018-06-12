e5872f13c4f96d2a8883996537d08bd02d53a581
andrea-velasquez <andrea.velasquez@utec.edu.pe>
2018-06-12 14:12:30 -0500

    Add token

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ def login_page():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/js/datos.js
+++ static/js/datos.js
@@ -1,11 +1,27 @@
-$.getJSON('http://innovations.pe/arturo/api-bweather/measure/read_all.php', function(data) {
+function main() {
 
-    //Muestra ultima actualizacion
-    $("#actualizacion").text("Última actualización: " + data["records"][data["records"].length-1]["created"]);
-    //Muestra la ultima lectura
-    $("#temperatura").text(data["records"][data["records"].length-1]["temperature"]+" ºC");
-    $("#humedad").text(data["records"][data["records"].length-1]["rel_humidity"]+"%");
+  var token;
+
+  $.getJSON('http://www.innovations.pe/arturo/api-bweather/api_key/create/', function(data1) {
+      token = data1["result"]["api_key"];
+      $("#token").text(token);
+
+  });
+
+  var url = "http://innovations.pe/arturo/api-bweather/measure/read_all.php";
+  var send = JSON.stringify({"api_key": "Uk1Ab2nvLvskmod"})
+
+  var posting = $.post( url, send, function(measures){
+    $("#actualizacion").text("Última actualización: " + measures["records"][measures["records"].length-1]["created"]);
+    $("#temperatura").text(measures["records"][measures["records"].length-1]["temperature"]+" ºC");
+    $("#humedad").text(measures["records"][measures["records"].length-1]["rel_humidity"]+"%");
     //Falta presion y humedad
-    $("#presion").text(data["records"][data["records"].length-1]["key-de-presion"]+"%");
-    $("#uv").text(data["records"][data["records"].length-1]["key-de-uv"]+"%");
-});
+    $("#presion").text(measures["records"][measures["records"].length-1]["key-de-presion"]+"%");
+    $("#uv").text(measures["records"][measures["records"].length-1]["key-de-uv"]+"%");
+  });
+
+  posting.fail(function() {
+    alert( "error" );
+  });
+}
+$(document).ready(main);
